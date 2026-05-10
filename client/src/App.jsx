959fa36ca0986d81fb9a5d6aa4bfdcafd3c07f9d
vibhamadabushi <madabushivibha@gmail.com>
2026-05-10 14:35:35 +0530

    activity search

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import CTA from "./components/CTA";
-import Footer from "./components/Footer";
-import CreateTrip from "./components/CreateTrip";
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import Features from "./components/Features";
+// import ActivitySearch from "./components/ActivitySearch";
+// import CTA from "./components/CTA";
+// import Footer from "./components/Footer";
+// import CreateTrip from "./components/CreateTrip";
 
-export default function App() {
-  return (
-    <div className="bg-[#F5F3F2] overflow-x-hidden">
-      <Navbar />
-      <Hero />
-      <Features />
-      <CreateTrip />
-      <CTA />
-      <Footer />
-    </div>
-  );
-}
+// export default function App() {
+//   return (
+//     <div className="bg-[#F5F3F2] overflow-x-hidden">
+//       <Navbar />
+//       <Hero />
+//       <Features />
+//       <CreateTrip />
+//        <ActivitySearch />
+//       <CTA />
+//       <Footer />
+//     </div>
+//   );
+// }
 
 // import CreateTrip from "./components/CreateTrip";
 
@@ -27,3 +29,13 @@ export default function App() {
 //     </div>
 //   );
 // }
+
+import ActivitySearch from "./components/ActivitySearch";
+
+export default function App() {
+  return (
+    <div className="bg-[#F5F3F2] min-h-screen">
+      <ActivitySearch />
+    </div>
+  );
+}
