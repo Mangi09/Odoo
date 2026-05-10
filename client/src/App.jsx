495c68ac60a5d0e9534dd14d8428eb7fddbbdea7
Keerthana <keert_207@yahoo.com>
2026-05-10 15:48:57 +0530

    adminlayout and mainlayout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,30 @@
-import { useState } from 'react'
-import Navbar from "./components/Navbar";
-import { Route, Routes } from 'react-router-dom'
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
 
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+import MainLayout from "./layouts/MainLayout";
+import AdminLayout from "./layouts/AdminLayout";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-    </>
-  )
+    <Routes>
+
+      {/* Normal Website Layout */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+
+      {/* Admin Layout */}
+      <Route element={<AdminLayout />}>
+
+      </Route>
+
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/AdminNavbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AdminNavbar = () => {
+    return (
+        <nav className="w-full px-6 lg:px-12 py-6 flex items-center justify-between max-w-7xl mx-auto z-50">
+
+        <h1 className="text-3xl font-bold text-[#2D2D2D]">
+            Traveloop
+        </h1>
+
+       </nav>
+    );
+}
+
+export default AdminNavbar
